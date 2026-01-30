a53974e13ed757e5e5531cf906fe4c6ef0ebcde7
hanz <hi@hanz.dev>
2026-01-30 08:02:15 +0700

    fix: naming convention and remove unused imports

--- test/GRAM.t.sol
+++ test/GRAM.t.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {GRAM} from "../src/GRAM.sol";
-import {console2} from "forge-std/console2.sol";
 import {MockXAUT} from "./MockXAUT.sol";
 
 contract GRAMTest is Test {
--- test/MockXAUT.sol
+++ test/MockXAUT.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.13;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockXAUT is ERC20 {
-    uint8 private constant _decimals = 8;
+    uint8 private constant DECIMALS = 8;
 
     constructor() ERC20("Tether Gold", "XAUT") {}
 
     function decimals() public view override returns (uint8) {
-        return _decimals;
+        return DECIMALS;
     }
 
     function mint(address to, uint256 amount) external {
